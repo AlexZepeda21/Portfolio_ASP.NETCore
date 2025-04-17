54a32c3484be9c07b802b88e31b2f93ea0952976
AlexZepeda21 <manuelzepedagr@gmail.com>
2025-04-17 01:24:23 -0600

    Migraciones usando Core.tools y base de datos MySQL con Pomelo generadas con éxito

---
+++ Portfolio/Migrations/20250417072148_Migrations.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ProjectCategory",
+                columns: table => new
+                {
+                    IdProjectCategory = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NameCategory = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ImageCategory = table.Column<byte[]>(type: "LONGBLOB", nullable: true),
+                    Description = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true, defaultValue: "Agregue una descripción")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectCategory", x => x.IdProjectCategory);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Technology",
+                columns: table => new
+                {
+                    IdTechnology = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NameTechnology = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ImageIcon = table.Column<byte[]>(type: "LONGBLOB", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Technology", x => x.IdTechnology);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    IdProject = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "varchar(2000)", maxLength: 2000, nullable: true, defaultValue: "Agregue una descripción")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ImageProject = table.Column<byte[]>(type: "LONGBLOB", nullable: true),
+                    ProjectCategoryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.IdProject);
+                    table.ForeignKey(
+                        name: "FK_Project_ProjectCategory_ProjectCategoryId",
+                        column: x => x.ProjectCategoryId,
+                        principalTable: "ProjectCategory",
+                        principalColumn: "IdProjectCategory",
+                        onDelete: ReferentialAction.SetNull);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    IdImage = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProjectId = table.Column<int>(type: "int", nullable: true),
+                    ImageBin = table.Column<byte[]>(type: "LONGBLOB", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.IdImage);
+                    table.ForeignKey(
+                        name: "FK_Image_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "IdProject",
+                        onDelete: ReferentialAction.SetNull);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ProjectTechnology",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(type: "int", nullable: false),
+                    TechnologyId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectTechnology", x => new { x.ProjectId, x.TechnologyId });
+                    table.ForeignKey(
+                        name: "FK_ProjectTechnology_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "IdProject",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProjectTechnology_Technology_TechnologyId",
+                        column: x => x.TechnologyId,
+                        principalTable: "Technology",
+                        principalColumn: "IdTechnology",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_ProjectId",
+                table: "Image",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_ProjectCategoryId",
+                table: "Project",
+                column: "ProjectCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectTechnology_TechnologyId",
+                table: "ProjectTechnology",
+                column: "TechnologyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Image");
+
+            migrationBuilder.DropTable(
+                name: "ProjectTechnology");
+
+            migrationBuilder.DropTable(
+                name: "Project");
+
+            migrationBuilder.DropTable(
+                name: "Technology");
+
+            migrationBuilder.DropTable(
+                name: "ProjectCategory");
+        }
+    }
+}
